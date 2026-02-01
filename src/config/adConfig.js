4fa3fc92644fe274cdd8d2720337206afa810178
BlackLabelBranding <lance@garzaunlimited.com>
2026-02-01 09:07:56 -0600

    Add adConfig.js with advertisement configuration

---
+++ src/config/adConfig.js
@@ -0,0 +1,20 @@
+export const ADS_CONFIG = [
+  {
+    id: 'main-logo',
+    imageUrl: 'https://horizons-cdn.hostinger.com/152da302-e677-4ba6-a3c4-c4aee9ad3690/6d6adf999c99e65188e5771513164be2.png',
+    link: null,
+    altText: 'Black Label Entertainment'
+  },
+  {
+    id: 'promo-venue',
+    imageUrl: 'https://horizons-cdn.hostinger.com/152da302-e677-4ba6-a3c4-c4aee9ad3690/6d6adf999c99e65188e5771513164be2.png', // Using same image for demo purposes
+    link: '/fans/submit',
+    altText: 'Are you a Venue? List your shows with us!'
+  },
+  {
+    id: 'promo-artist',
+    imageUrl: 'https://horizons-cdn.hostinger.com/152da302-e677-4ba6-a3c4-c4aee9ad3690/6d6adf999c99e65188e5771513164be2.png', // Using same image for demo purposes
+    link: '/fans/submit',
+    altText: 'Artists: Get discovered on Black Label'
+  }
+];
